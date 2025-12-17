b31263c2e871096488b9839de8bd45e32c3f2294
yukitoame <akaisuki2@gmail.com>
2025-12-17 19:44:15 +0900

    main.go:add functions for get and calcrate

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
+import "fmt"
+
 const usdToEurRate = 0.85
 const usdToRubRate = 79.51
 const eurToRubRate = usdToRubRate / usdToEurRate
 
 func main() {
+
+}
+
+func getUserInput() (input string) {
+	fmt.Scan(&input)
+	return
+}
+
+func calculateRate(number float64, firstRate string, secondRate string) {
 
 }
