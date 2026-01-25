e25911d89b962a173edf44c63265c944eda2479c
yukitoame <akaisuki2@gmail.com>
2026-01-25 15:04:09 +0900

    change calcrate func

--- main.go
+++ main.go
@@ -20,16 +20,15 @@ const (
 	rubToEurRate float64 = rubToUsdRate / eurToUsdRate
 )
 
-var rates = map[string]float64{
-	"USD-RUB": usdToRubRate,
-	"USD-EUR": usdToEurRate,
-	"EUR-RUB": eurToRubRate,
-	"EUR-USD": eurToUsdRate,
-	"RUB-USD": rubToUsdRate,
-	"RUB-EUR": rubToEurRate,
-}
-
 func main() {
+	rates := map[string]float64{
+		"USD-RUB": usdToRubRate,
+		"USD-EUR": usdToEurRate,
+		"EUR-RUB": eurToRubRate,
+		"EUR-USD": eurToUsdRate,
+		"RUB-USD": rubToUsdRate,
+		"RUB-EUR": rubToEurRate,
+	}
 	for {
 		firstCurrency, err := getUserFirstCurrency()
 		if errorCheck(err) {
@@ -46,7 +45,7 @@ func main() {
 			continue
 		}
 
-		result := calculateRate(quantityCurrency, firstCurrency, lastCurrency)
+		result := calculateRate(&rates, quantityCurrency, firstCurrency, lastCurrency)
 		fmt.Printf("%.0f %s = %.2f %s\n", quantityCurrency, firstCurrency, result, lastCurrency)
 		if !checkRepeat() {
 			break
@@ -128,9 +127,9 @@ func getUserLastCurrency(currency string) (input string, errInput error) {
 	return input, nil
 }
 
-func calculateRate(number float64, from, to string) float64 {
-	key := from + "-" + to                  // Ключ для поиска по мапе, мы в мапе сделали ключи как "RUB-USD"
-	if rate, exists := rates[key]; exists { // если exists тру то мы выполняем тело иф
+func calculateRate(rates *map[string]float64, number float64, from, to string) float64 {
+	key := from + "-" + to                     // Ключ для поиска по мапе, мы в мапе сделали ключи как "RUB-USD"
+	if rate, exists := (*rates)[key]; exists { // если exists тру то мы выполняем тело иф
 		return number * rate // выполняется если ключ существует в мапе
 	}
 	return 0
